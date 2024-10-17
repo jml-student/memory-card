8d57fc2c9ebb1ca4776f1fd448a592020d6c73e8
jml <student741@proton.me>
2024-10-17 11:00:28 +0200

    Create componentes and reset index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Header from './components/Header'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Header />
+    <Cards />
+    <Footer />
+  </StrictMode>
 )
